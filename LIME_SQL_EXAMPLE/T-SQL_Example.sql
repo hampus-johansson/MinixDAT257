b4d295b017f9c05492d71884f97b6322e82c2760
hampusctk <66749787+hampusctk@users.noreply.github.com>
2021-04-19 16:41:14 +0200

    Update T-SQL_Example.sql
    Co-Authored-By: AmandaAll <74770486+AmandaAll@users.noreply.github.com>

--- LIME_SQL_EXAMPLE/T-SQL_Example.sql
+++ LIME_SQL_EXAMPLE/T-SQL_Example.sql
@@ -1,3 +1,4 @@
+
 /*
   UNDERSTANDING THE Show Advanced Options
   ------------------------------------------------------------------------------------------------------------------
@@ -44,15 +45,20 @@ DECLARE @apiKey NVARCHAR(32);
 -- Variable declaration related to the JSON string.
 DECLARE @json AS TABLE(Json_Table NVARCHAR(MAX))
 
--- Set Authentications
-SET @authHeader = '38a0b5ee-95eb-4fe4-897c-3678791d9478';
-SET @contentType = 'event/feed/';
+-- Set the API Key
+SET @apiKey = 'c6f6e65b-0379-4bfb-bd06-a7deac89df68';
 
--- Set the API Key, I'm just grabbing it from another table in my Database.
-SET @apiKey = ''
+-- Set Authentications
+SET @authHeader = 'go-api:'+@apiKey;
+SET @contentType = 'application/json';
+
+
+
+--INSERT into @json (Json_Table) VALUES (@token);
 
 -- Define the URL
-SET @url = 'https://api.lime-go.com/v1/'
+SET @url = 'https://api.lime-go.com/v1/event/feed/?peek=true&data=[out:json]&apikey=c6f6e65b-0379-4bfb-bd06-a7deac89df68
+';
 
 -- This creates the new object.
 EXEC @ret = sp_OACreate 'MSXML2.XMLHTTP', @token OUT;
@@ -60,8 +66,8 @@ IF @ret <> 0 RAISERROR('Unable to open HTTP connection.', 10, 1);
 
 -- This calls the necessary methods.
 EXEC @ret = sp_OAMethod @token, 'open', NULL, 'GET', @url, 'false';
-EXEC @ret = sp_OAMethod @token, 'setRequestHeader', NULL, 'Authentication', @authHeader;
-EXEC @ret = sp_OAMethod @token, 'setRequestHeader', NULL, 'Content-type', @contentType;
+--EXEC @ret = sp_OAMethod @token, 'setRequestHeader', NULL, 'Authorization', @authHeader;
+--EXEC @ret = sp_OAMethod @token, 'setRequestHeader', NULL, 'Content-type', @contentType;
 EXEC @ret = sp_OAMethod @token, 'send'
 
 -- Grab the responseText property, and insert the JSON string into a table temporarily. This is very important, if you don't do this step you'll run into problems.
@@ -70,78 +76,14 @@ INSERT into @json (Json_Table) EXEC sp_OAGetProperty @token, 'responseText'
 -- Select the JSON string from the Table we just inserted it into. You'll also be able to see the entire string with this statement.
 SELECT * FROM @json
 
+IF OBJECT_ID('events', 'U') IS NOT NULL
+DROP TABLE events
 
-/*
-	Okay, at this point we have a JSON string we can begin to parse. To parse this JSON string we will use the OPENJSON() function used in T-SQL.
-	From here, we will begin unraveling the JSON one level at a time. This is often the most confusing part as JSON strings can be extremely nested.
-	The way to approach this is to start from the TOP of the string and work your way in.
-	Additionally, we will leverage the CROSS APPLY function in order to merge all the results of our parsing to a single table. When looking at the code
-	below it might look a little backwards. For example, the top SELECT statement is referencing all the columns we unpacked and each individual WITH
-	statment below it seems to be referencing a certain scetion of the JSON string.
-*/
-
-
--- Display all the data we just parsed, keep in mind you can negate certain columns we parsed. There is no requirement to display all the columns.
 SELECT 
+	*
+into events FROM OPENJSON((SELECT * FROM @json))   -- USE OPENJSON to begin the parse.
+	WITH (
+		[firstName] NVARCHAR(MAX) '$.items'
+	)
 
-	[Time_Series_Metadata].[2. Symbol] AS Ticker_Symbol,
-	[Time_Series_Metadata].[4. Interval] AS Time_Interval,
-	[Time_Series_Date].[key] AS Date_ID,
-	[PriceData].[1. open] AS Open_Price,
-    [PriceData].[2. high] AS High_Price,
-    [PriceData].[3. low] AS Low_Price,
-    [PriceData].[4. close] AS Close_Price,
-    [PriceData].[5. volume] AS Voume
-
-FROM OPENJSON((SELECT * FROM @json))  -- USE OPENJSON to begin the parse.
-
--- At the highest level we have two parts, the `Meta Data` and the `Time Series (5min)` dictionaries.
-WITH (
-	[Time Series (5min)] NVARCHAR(MAX) AS JSON,
-	[Meta Data] NVARCHAR(MAX) AS JSON
-) AS  MetaData
-
-
-/*
-	Okay at this point we have two pieces of the bigger document. Let's start with the `Meta Data` portion 
-	because that's easiest to parse.
-*/
-
--- Parse the Metadata
-CROSS APPLY OPENJSON([MetaData].[Meta Data])
-WITH(
-    [1. Information] NVARCHAR(MAX),
-    [2. Symbol] NVARCHAR(MAX),
-    [3. Last Refreshed"] NVARCHAR(MAX),
-    [4. Interval] NVARCHAR(MAX),
-    [5. Output Size] NVARCHAR(MAX),
-    [6. Time Zone] NVARCHAR(MAX)
-) AS Time_Series_Metadata
-
-
-/*
-	With the `Meta Data` out of the way we can proceed to the `Time Series (5 min)` portion. This is
-	a little challenging as each key inside this dictionary of dictionaries is unique. They are timestamps.
-	Normally, we just would specify the portion we want by defining the section `Time Series (5 min)` followed
-	by the date. In essence it would look like this:
-		`Time Series (5 min)`.`20190101110012`
-	The problem is we could have a 100 of these dictionaries each one having a different time stamp. It's not
-	realistic to write each one out. What we could do is first grab all the all keys and then grab all the values
-	for each key using CROSS APPLY.
-*/
-
-
--- The tricky part, we need to go through each time series JSON array, and grab the key, value, and type.
-CROSS APPLY OPENJSON([MetaData].[Time Series (5min)]) AS Time_Series_Date
-
--- Then take ONLY THE VALUE part and parse it further, as this is where the data is.
-CROSS APPLY OPENJSON([Time_Series_Date].[value])
-WITH(
-	
-    [1. open] NVARCHAR(MAX),
-    [2. high] NVARCHAR(MAX),
-    [3. low] NVARCHAR(MAX),
-    [4. close] NVARCHAR(MAX),
-    [5. volume] NVARCHAR(MAX)
-
-) AS PriceData
+SELECT * FROM events;
