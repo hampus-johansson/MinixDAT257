27dcc6013b19724d876c0cda9a6cf41e8d2a66a0
Victorcousin <57449953+Victorcousin@users.noreply.github.com>
2021-04-21 15:58:48 +0200

    Skapat tables i Microsoft SQL
    Co-Authored-By: erikaherbai <81307902+erikaherbai@users.noreply.github.com>
    Co-Authored-By: linda-e <81307878+linda-e@users.noreply.github.com>
    
    #2 #5

---
+++ Databas/setupQuery.sql
@@ -0,0 +1,52 @@
+CREATE TABLE Users (
+email varchar(100) NOT NULL,
+uname text NOT NULL,
+CONSTRAINT constraintName PRIMARY KEY (email)
+);
+CREATE TABLE ClockifyUser(
+	email varchar(100) NOT NULL,
+	workedHours REAL NOT NULL,
+	clockID TEXT NOT NULL,
+	CONSTRAINT ClockifyPrimaryKey PRIMARY KEY (email),
+	CONSTRAINT UniqueUser UNIQUE (email,workedHours), 
+	CONSTRAINT foreignKey FOREIGN KEY (email) REFERENCES Users (email)
+);
+CREATE TABLE LimeGoUser(
+	email varchar(100) NOT NULL,
+	salesMeetings INTEGER NOT NULL,
+	limeID TEXT NOT NULL,
+	CONSTRAINT LimeGoPrimaryKey PRIMARY KEY (email),
+	CONSTRAINT UniqueUserSales UNIQUE (email,salesMeetings),
+	CONSTRAINT LimeGoforeignKey FOREIGN KEY (email) REFERENCES Users (email)
+);
+CREATE TABLE API (
+	APIkey varchar(100) NOT NULL, 
+	CONSTRAINT APIPrimaryKey PRIMARY KEY (APIkey)
+);
+CREATE TABLE Program(
+	pname varchar(100) NOT NULL, 
+	APIkey varchar(100) NOT NULL UNIQUE,
+	CONSTRAINT ProgramPrimaryKey PRIMARY KEY (pname), 
+	CONSTRAINT ProgramforeignKey FOREIGN KEY (APIkey) REFERENCES API (APIkey)
+);
+CREATE TABLE Relation (
+	email varchar(100) NOT NULL,
+	workedHours REAL NOT NULL, 
+	salesMeetings INTEGER NOT NULL,
+	CONSTRAINT RelationPrimaryKey PRIMARY KEY (email),
+	CONSTRAINT RelationforeignKey FOREIGN KEY (email) REFERENCES Users (email),
+	CONSTRAINT RelationforeignKeyOne FOREIGN KEY (email, workedHours) REFERENCES ClockifyUser (email,workedHours),
+	CONSTRAINT RelationforeignKeyTwo FOREIGN KEY (email, salesMeetings) REFERENCES LimeGoUser (email,salesMeetings)
+);
+CREATE TABLE Connections(
+	email varchar(100) NOT NULL,
+	pname varchar(100) NOT NULL, 
+	workedHours REAL NOT NULL,
+	salesMeetings INTEGER NOT NULL,
+	APIkey varchar(100) NOT NULL, 
+	CONSTRAINT ConnectionsPrimaryKey PRIMARY KEY (email,pname),
+	CONSTRAINT ConncetionsforeignKey FOREIGN KEY (email) REFERENCES Users (email),
+	CONSTRAINT ConnectionsforeignKeyOne FOREIGN KEY (email, workedHours) REFERENCES ClockifyUser (email,workedHours),
+	CONSTRAINT ConnectionsforeignKeyTwo FOREIGN KEY (email, salesMeetings) REFERENCES LimeGoUser (email,salesMeetings),
+	CONSTRAINT ConnectionsforeignKey FOREIGN KEY (APIkey) REFERENCES Program (APIkey)
+);
