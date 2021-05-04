8ca68fb5fd134be010b26baef5850c86fcb32cd0
hampusctk <66749787+hampusctk@users.noreply.github.com>
2021-05-04 18:35:48 +0200

    Done
    Co-Authored-By: erikaherbai <81307902+erikaherbai@users.noreply.github.com>
    Co-Authored-By: glaskula <42672254+glaskula@users.noreply.github.com>

--- Setup/setupMain.sql
+++ Setup/setupMain.sql
@@ -1,6 +1,7 @@
 -- Måste aktivera SQLCMD Mode i Query-fliken
 --CREATE DATABASE Konsulttrappan
 
+-- Skriv in sökvägen till dessa filer på din dator
 --:r C:\Users\Hampus\Documents\GitHub\MinixDAT257\Setup\setupQuery.sql
 --:r C:\Users\Hampus\Documents\GitHub\MinixDAT257\Setup\InsertIntoClockifyUser.sql
 --:r C:\Users\Hampus\Documents\GitHub\MinixDAT257\Setup\InsertIntoLimeGoUser.sql
