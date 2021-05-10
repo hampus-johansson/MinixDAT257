8ce8d9ff5a806950f2c2f6a0c86a527df3e6d8c5
Philip Rabia <philip.rabia@hotmail.com>
2021-05-10 12:03:09 +0200

    Itkonsult fixed
    Co-Authored-By: oswalchlm <81307947+oswalchlm@users.noreply.github.com>

--- Main/Setup/setupMain.sql
+++ Main/Setup/setupMain.sql
@@ -1,10 +1,17 @@
 -- Måste aktivera SQLCMD Mode i Query-fliken
 --CREATE DATABASE Konsulttrappan
---Insert into API(pname,APIkey) Values ('LimeGo', <NYCKEL>)
---Insert into API(pname,APIkey) Values ('Clockify', <NYCKEL>)
 
+
+-- 1
+-- Kör först denna
+--:r C:\Users\phili\OneDrive\Dokument\GitHub\MinixDAT257\Main\Setup\setupQuery.sql 
+
+-- 2
+--Insert into API(pname,APIkey) Values ('LimeGo', 'fd8b0d17-e940-4d0c-bae9-34f8ee6bb74f')
+--Insert into API(pname,APIkey) Values ('Clockify', 'YjUxZGZiMWUtMmY2My00NTNhLTk4ODMtYWIzYmI3M2ZjNDRh')
+
+-- 3
 -- Skriv in sökvägen till dessa filer på din dator
---:r C:\Users\Hampus\Documents\GitHub\MinixDAT257\Setup\setupQuery.sql
---:r C:\Users\Hampus\Documents\GitHub\MinixDAT257\Setup\InsertIntoClockifyUser.sql
---:r C:\Users\Hampus\Documents\GitHub\MinixDAT257\Setup\InsertIntoLimeGoUser.sql
---:r C:\Users\Hampus\Documents\GitHub\MinixDAT257\Setup\inserttorelations.sql
+--:r C:\Users\phili\OneDrive\Dokument\GitHub\MinixDAT257\Main\Setup\InsertIntoClockifyUser.sql
+--:r C:\Users\phili\OneDrive\Dokument\GitHub\MinixDAT257\Main\Setup\InsertIntoLimeGoUser.sql
+--:r C:\Users\phili\OneDrive\Dokument\GitHub\MinixDAT257\Main\Setup\inserttorelations.sql
